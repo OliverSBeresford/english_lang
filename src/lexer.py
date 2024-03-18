a273b7052a4ec0f8a89f2eb265f939da867a5443
Oliver Beresford <oggynaut@icloud.com>
2024-03-17 20:13:37 -0400

    Added starting code for lexer.py

---
+++ src/lexer.py
@@ -0,0 +1,64 @@
+import re
+
+class Token:
+    def __init__(self, type_, value):
+        self.type = type_
+        self.value = value
+
+    def __repr__(self):
+        return f'Token({self.type}, {self.value})'
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+
+    def advance(self):
+        self.pos += 1
+
+    def get_next_token(self):
+        text = self.text
+
+        if self.pos >= len(text):
+            return Token('EOF', None)
+
+        current_char = text[self.pos]
+
+        if current_char.isspace():
+            self.skip_whitespace()
+            return self.get_next_token()
+
+        if current_char.isalpha() or current_char == '"':
+            return self.word()
+
+        if current_char.isdigit():
+            return self.number()
+
+        self.error()
+
+    def skip_whitespace(self):
+        while self.pos < len(self.text) and self.text[self.pos].isspace():
+            self.advance()
+
+    def word(self):
+        result = ''
+        while self.pos < len(self.text) and (self.text[self.pos].isalpha() or self.text[self.pos] in ['"', ' ']):
+            result += self.text[self.pos]
+            self.advance()
+        return Token('WORD', result)
+
+    def number(self):
+        result = ''
+        while self.pos < len(self.text) and self.text[self.pos].isdigit():
+            result += self.text[self.pos]
+            self.advance()
+        return Token('NUMBER', result)
+
+    def error(self):
+        raise Exception('Invalid character')
+
+    def tokenize(self):
+        tokens = []
+        while (token := self.get_next_token()).type != 'EOF':
+            tokens.append(token)
+        return tokens
