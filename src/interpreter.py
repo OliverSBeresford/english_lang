eda823a2ef5020057b6e0a9c09b2bb8c4af0bf3c
Oliver Beresford <oggynaut@icloud.com>
2024-04-18 20:13:33 -0500

    Corrected indent error in interpreter

--- src/interpreter.py
+++ src/interpreter.py
@@ -19,36 +19,36 @@ class Interpreter:
             self.visit(node)
 
 
-def visit_VariableAssignment(self, node):
-    # Assume we have a dictionary called self.variables to store variable values
-    self.variables[node.variable_name] = self.visit(node.value)
+    def visit_VariableAssignment(self, node):
+        # Assume we have a dictionary called self.variables to store variable values
+        self.variables[node.variable_name] = self.visit(node.value)
 
-# Update the interpret method to handle variable assignments
-# When a VariableAssignment node is encountered, call visit_VariableAssignment
+    # Update the interpret method to handle variable assignments
+    # When a VariableAssignment node is encountered, call visit_VariableAssignment
 
-# Example usage
-# Assuming the AST is generated from the parser for the input "print 'Hello, World!'"
-"""ast = [PrintStatement('"Hello, World!"')]
-interpreter = Interpreter(ast)
-interpreter.interpret()"""
-# Output: Hello, World!
-def visit_IfStatement(self, node):
-    variable_value = self.variables.get(node.condition[0])
-    if variable_value == node.condition[1]:
-        self.visit(node.true_branch)
-    elif node.false_branch:
-        self.visit(node.false_branch)
+    # Example usage
+    # Assuming the AST is generated from the parser for the input "print 'Hello, World!'"
+    """ast = [PrintStatement('"Hello, World!"')]
+    interpreter = Interpreter(ast)
+    interpreter.interpret()"""
+    # Output: Hello, World!
+    def visit_IfStatement(self, node):
+        variable_value = self.variables.get(node.condition[0])
+        if variable_value == node.condition[1]:
+            self.visit(node.true_branch)
+        elif node.false_branch:
+            self.visit(node.false_branch)
 
-def visit_LoopStatement(self, node):
-    for _ in range(node.iterations):
-        self.visit(node.body)
+    def visit_LoopStatement(self, node):
+        for _ in range(node.iterations):
+            self.visit(node.body)
 
-### Functions
-def visit_FunctionDefinition(self, node):
-    self.functions[node.name] = node.body
+    ### Functions
+    def visit_FunctionDefinition(self, node):
+        self.functions[node.name] = node.body
 
-def visit_FunctionCall(self, node):
-    if node.name in self.functions:
-        self.visit(self.functions[node.name])
-    else:
-        raise Exception(f"Function {node.name} not defined")
+    def visit_FunctionCall(self, node):
+        if node.name in self.functions:
+            self.visit(self.functions[node.name])
+        else:
+            raise Exception(f"Function {node.name} not defined")
